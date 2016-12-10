c37baf81f1ba9159a9106f43ebfd149ff50f7b11
Eldon Nelson <eldon_nelson@ieee.org>
2016-12-09 21:44:04 -0800

    add in perf_1.sh

---
+++ scripts/perf_1.sh
@@ -0,0 +1,15 @@
+make clean
+make shutdown
+make build_synopsys WIDTH=5
+
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
+make -j1 sim_synopsys_parallel_perf SERVER=127.0.0.1 PARALLEL_SIMS=1
